beca4de8c2b213b0ee31873ef1a9775b4e8517cf
bling-yshs <377178599@qq.com>
2024-04-19 18:37:46 +0800

    change

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ async function run() {
     actionInput.retry = 3
   }
   core.info(`值为${actionInput.retry}，路径为${actionInput.path}`)
-  const info = JSON.stringify(github.context.repo)
+  const info = JSON.stringify(github.context)
   core.info(`信息：${info}`)
   core.info('end')
 }
