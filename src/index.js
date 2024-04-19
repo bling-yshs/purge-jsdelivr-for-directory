694393e3ef19ee07268c741f48a87cdd972150d3
bling-yshs <377178599@qq.com>
2024-04-19 18:09:11 +0800

    try

--- src/index.js
+++ src/index.js
@@ -1,6 +1,28 @@
-/**
- * The entrypoint for the action.
- */
-const { run } = require('./main')
+const core = require('@actions/core')
+const github = require('@actions/github')
 
-run()
+// read action inputs
+const actionInput = {
+  path: core
+    .getInput('path', { required: true })
+    .split('\n')
+    .map(url => url.trim()),
+  retry: parseInt(core.getInput('retry'), 10)
+}
+
+async function run() {
+  // processInput
+  if (Number.isNaN(actionInput.retry) || actionInput.retry <= 0) {
+    actionInput.retry = 3
+  }
+  core.info(`值为${actionInput.retry}，路径为${actionInput.path}`)
+  const info = JSON.stringify(github.context.repo)
+  core.info(`信息：${info}`)
+}
+
+// run the action
+try {
+  run()
+} catch (error) {
+  core.setFailed(error.message)
+}
