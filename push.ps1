b7c86cb7f48f1fbccffe65a7e4930ca1cdc50339
bling-yshs <377178599@qq.com>
2024-04-21 23:44:50 +0800

    test

--- push.ps1
+++ push.ps1
@@ -1,7 +1,7 @@
 npm run all
 git add .
 git commit -m "test"
-git tag -d v1.0
-git push --delete origin v1.0
-git tag -a -m "test" v1.0
+git tag -d v1.0.0
+git push --delete origin v1.0.0
+git tag -a -m "test" v1.0.0
 git push --follow-tags -f
